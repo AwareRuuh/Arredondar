9d7404ee887b97dba8428860417643e72b4727dc
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-11-19 12:26:36 +0000

    Utilizo o " import java.math.*; " para chamar o BigDecimal

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.math.*;
+// ^ utilizei para importar o BigDecimal.
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+  double d = 451.3256531;
+BigDecimal bd = new BigDecimal(d).setScale(4, RoundingMode.HALF_EVEN);
+System.out.println(bd.doubleValue());
   }
 }
